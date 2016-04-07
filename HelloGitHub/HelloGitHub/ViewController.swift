b32dbc151161ae934c3148fae7767a3784c099a8
Xu, Jin(Billy) <billyxu@cienet.com.cn>
2016-04-07 16:14:33 +0800

    remove UserInterfaceState.xcuserstate

--- HelloGitHub/HelloGitHub/ViewController.swift
+++ HelloGitHub/HelloGitHub/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         
         
         print("commit test")
+        
+        print("UserInterfaceState.xcuserstate test")
     }
 
     override func didReceiveMemoryWarning() {
